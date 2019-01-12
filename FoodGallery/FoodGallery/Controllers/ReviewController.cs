9be9884b63eed0e72a8165314068791ee0cae547
SarkerS <sibendu.srkr@gmail.com>
2019-01-12 15:54:31 -0600

    ReviewPage created

---
+++ FoodGallery/FoodGallery/Controllers/ReviewController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FoodGallery.Models;
+
+
+namespace FoodGallery.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+            var datar = from r in _localdata
+                        orderby r.City
+                        select r;
+
+            return View(datar);
+        }
+
+        // GET: Review/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Review/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        static List<RestaurentReview> _localdata = new List<RestaurentReview>() {
+
+        new RestaurentReview { Id = 1, Name = "Five Gays", City = "Winnipeg", Country = "Canada", Rating = 7.5},
+        new RestaurentReview { Id = 2, Name = "Tim Horton", City = "Rgina", Country = "Bangladesh", Rating = 8.5}
+
+
+        };
+
+
+    }
+
+
+
+
+
+
+}
---
+++ FoodGallery/FoodGallery/Models/RestaurentReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodGallery.Models
+{
+    public class RestaurentReview
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public double Rating { get; set; }
+
+    }
+}
