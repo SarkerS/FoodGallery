e5e71ecac1cf2987e6eabf46cba601214ce29ea1
SarkerS <sibendu.srkr@gmail.com>
2019-01-12 15:13:51 -0600

    Initial Commit

---
+++ FoodGallery/FoodGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodGallery.Startup))]
+namespace FoodGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
